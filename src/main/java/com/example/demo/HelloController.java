0e7c34f7450a2de8d146c2b0e5327e34629eac6a
Craig Walls <craig@habuma.com>
2017-10-27 12:53:27 -0600

    Build against Boot 2.0.0 and Security 5.0.0 snapshots

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import org.springframework.security.oauth2.client.authentication.OAuth2UserAuthenticationToken;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 	
 	@GetMapping("/")
-	public String hello(OAuth2UserAuthenticationToken token, Model model) {
+	public String hello(OAuth2AuthenticationToken<?,?> token, Model model) {
 		DefaultOAuth2User principal = (DefaultOAuth2User) token.getPrincipal();
 		model.addAttribute("who", principal.getName());
 		return "greeting";
